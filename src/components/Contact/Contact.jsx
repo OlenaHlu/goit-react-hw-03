a5b0042ff92d6cdac10c3a8f55946acada6c8274
OlenaHlu <glushenkova_es@ukr.net>
2024-05-06 22:07:07 +0300

    step1,2

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = ({ id, name, number }) => {
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{number}</p>
+      <button type="button">Delete</button>
+    </div>
+  );
+};
+
+export default Contact;
